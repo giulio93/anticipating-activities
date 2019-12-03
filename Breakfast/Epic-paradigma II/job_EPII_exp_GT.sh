32f707914417db2d0f8ef79036784d8c98c19ff0
giulio93 <giulio.pilotto@gmail.com>
2019-12-03 14:40:44 +0100

    update paradigam epii 2 breakfast

--- Breakfast/Epic-paradigma II/job_EPII_exp_GT.sh
+++ Breakfast/Epic-paradigma II/job_EPII_exp_GT.sh
@@ -4,16 +4,16 @@ source /venv/bin/activate
 # echo "START "
   
 echo "SPLIT 1"
-python3 main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=train --vid_list_file=./data/train.split1.bundle --model_save_path=./save_dir/models_6_8_0.50/rnn1 --results_save_path=./save_dir/results_6_8_0.50/rnn1 
+python main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=train --vid_list_file=./data/train.split1.bundle --model_save_path=./save_dir/models_6_8_0.50/rnn1 --results_save_path=./save_dir/results_6_8_0.50/rnn1 
 python main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=predict  --vid_list_file=./data/test.split1.bundle --model_save_path=./save_dir/models_6_8_0.50/rnn1 --results_save_path=./save_dir/results_6_8_0.50/rnn1  --input_type=gt
 echo "SPLIT 2"
-python3 main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=train --vid_list_file=./data/train.split2.bundle --model_save_path=./save_dir/models_6_8_0.50/rnn2 --results_save_path=./save_dir/results_6_8_0.50/rnn2 
+python main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=train --vid_list_file=./data/train.split2.bundle --model_save_path=./save_dir/models_6_8_0.50/rnn2 --results_save_path=./save_dir/results_6_8_0.50/rnn2 
 python main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=predict  --vid_list_file=./data/test.split2.bundle  --model_save_path=./save_dir/models_6_8_0.50/rnn2 --results_save_path=./save_dir/results_6_8_0.50/rnn2  --input_type=gt
 echo "SPLIT 3"
-python3 main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=train --vid_list_file=./data/train.split3.bundle --model_save_path=./save_dir/models_6_8_0.50/rnn3 --results_save_path=./save_dir/results_6_8_0.50/rnn3   
+python main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=train --vid_list_file=./data/train.split3.bundle --model_save_path=./save_dir/models_6_8_0.50/rnn3 --results_save_path=./save_dir/results_6_8_0.50/rnn3   
 python main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=predict  --vid_list_file=./data/test.split3.bundle  --model_save_path=./save_dir/models_6_8_0.50/rnn3 --results_save_path=./save_dir/results_6_8_0.50/rnn3   --input_type=gt
 echo "SPLIT 4"
-python3 main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=train --vid_list_file=./data/train.split4.bundle --model_save_path=./save_dir/models_6_8_0.50/rnn4 --results_save_path=./save_dir/results_6_8_0.50/rnn4 
+python main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=train --vid_list_file=./data/train.split4.bundle --model_save_path=./save_dir/models_6_8_0.50/rnn4 --results_save_path=./save_dir/results_6_8_0.50/rnn4 
 python main.py --model=rnn --S_enc=6 --S_ant=8 --beta=0.50 --action=predict  --vid_list_file=./data/test.split4.bundle  --model_save_path=./save_dir/models_6_8_0.50/rnn4 --results_save_path=./save_dir/results_6_8_0.50/rnn4  --input_type=gt
 
 
